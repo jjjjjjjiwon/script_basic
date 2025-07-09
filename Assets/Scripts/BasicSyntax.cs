f336f7a3fc92d65abc7d6fa14c181fdebe27580b
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-09 14:45:52 +0900

    0709_3

--- Assets/Scripts/BasicSyntax.cs
+++ Assets/Scripts/BasicSyntax.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class BasicSyntax : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(transform.position.x);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
     }
 }
