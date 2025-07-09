c5e69111eb1addf8c7c97ee4d7e2acc896aa2158
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-09 14:52:50 +0900

    Update BasicSyntax.cs

--- Assets/Scripts/BasicSyntax.cs
+++ Assets/Scripts/BasicSyntax.cs
@@ -6,11 +6,21 @@ public class BasicSyntax : MonoBehaviour
 {
     void Start()
     {
+        //이 행은 오브젝트의 x 위치를 나타냅니다.
+        /*
+        안녕하세요
+        두줄로 구성된 주석입니다.
+        */
         Debug.Log(transform.position.x);
-        
-        if(transform.position.y <= 5f)
+
+        if (transform.position.y <= 5f)
         {
-            Debug.Log ("I'm about to hit the ground!");
+            Debug.Log("I'm about to hit the gameObject");
+        }
+        if (transform.rotation.x < 4f)
+        {
+
         }
     }
+
 }
