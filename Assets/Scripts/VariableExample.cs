043225847c0c9b5f001c3c3c27d4b8aab0fa49c5
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-11 15:50:04 +0900

    Update VariableExample.cs

--- Assets/Scripts/VariableExample.cs
+++ Assets/Scripts/VariableExample.cs
@@ -1,14 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VariableExample : MonoBehaviour
 {
-
+    int globalNumber; // 전역 변수 , 초기화 필요없음, 기본값이 들어가있음
+    
     void Start()
     {
-        int number1;
-        number1 = 1;
+        int localNumber = 0; // 지역 변수, 초기화 필요
+        
+
+        Console.WriteLine(globalNumber);
+        Console.WriteLine(localNumber);
+
+        int nyumber1;
+        nyumber1 = 1;
         int number2 = 2;
         float number3 = 3.4f, number4 = 4f;
 
@@ -17,8 +25,8 @@ public class VariableExample : MonoBehaviour
         bool isNotTrue = true;
         bool isover = false;
 
-        Debug.Log(number1);
-        Debug.Log(number1.ToString());
+        Debug.Log(nyumber1);
+        Debug.Log(nyumber1.ToString());
         Debug.Log(number3);
         Debug.Log(number3.ToString());
         Debug.Log(isNotTrue);
