8ab8b417c812ba20979835d3c56156100010b21d
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-17 15:49:21 +0900

    0717

--- Assets/Scripts/old/VariableExample.cs
+++ Assets/Scripts/old/VariableExample.cs
@@ -9,9 +9,25 @@ public class VariableExample : MonoBehaviour
     
     void Start()
     {
-        int localNumber = 0; // 지역 변수, 초기화 필요
-        
 
+        var hexLiteral = 0xaf; // 16진수
+        var binaryLiteral = 0b_0000_0001; // 2진수 , _의미 없음 보기 편하라고
+
+        // 최대 값, 최소값
+        int mi = int.MaxValue;
+        int ni = int.MinValue;
+
+        bool b = default(bool);  // bool의 초기값은 false(0)
+
+        string s1 = null;   // stringd의 초기값은 null이다
+        string s2 = "";
+        string s3 = string.Empty;
+
+        // Nullabl - 참조형 형식이 아닌 변수에 null을 넣을수 있다, 입력이 없을때 사용가능
+        int? ii = null;
+        ii = 20;
+
+        int localNumber = 0; // 지역 변수, 초기화 필요
         Console.WriteLine(globalNumber);
         Console.WriteLine(localNumber);
 
