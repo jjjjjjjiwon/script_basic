28fae31271829b2ba8a91aaa1020596fba49a8a5
jjjjjjjiwon <hgw7501@gmail.com>
2025-07-18 15:47:40 +0900

    0718

---
+++ Assets/Scripts/TransformFunctions.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 1000f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+        }
+
+    }
+}
