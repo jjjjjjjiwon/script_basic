e1aaf37f8d8aceda1455bbff7db1eeaf927e217b
jjjjjjjiwon <hgw7501@gmail.com>
2025-08-04 15:51:11 +0900

    0804

---
+++ Assets/Scripts/FormatStringTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormatStringTest : MonoBehaviour
+{
+    void Start()
+    {
+        string[] names = { "Iron Man", "Super Man" };
+        int[] ages = { 55, 100 };
+
+        Debug.LogFormat({"{0, -20}"});
+    }
+}
---
+++ Assets/Scripts/MovementControls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControls : MonoBehaviour
+{
+
+    public float speed;
+    public float turnSpeed;
+
+    void MoveMent()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float turnMovement = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+    void Update()
+    {
+        MoveMent();
+    }
+}
